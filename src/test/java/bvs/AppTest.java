57e40f19620c9837e66af2a541ddc36dea4fe032
Ítalo O Costa <italo.de.oliveira01@gmail.com>
2019-03-14 22:44:28 -0300

    ADD diagramas.astah

--- src/test/java/bvs/AppTest.java
+++ src/test/java/bvs/AppTest.java
@@ -4,17 +4,11 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue() {
         assertTrue( true );
     }
+    
 }
