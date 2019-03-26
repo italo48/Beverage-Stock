26c52ee2349476b08e4952b3313b27facc9b9e72
lincoln <lincolnengsoft@gmail.com>
2019-03-26 18:46:38 -0300

    estoque e teste

--- src/main/java/bvs/controle/StockControl.java
+++ src/main/java/bvs/controle/StockControl.java
@@ -1,9 +1,39 @@
 package bvs.controle;
 
-public class StockControl {
+import bvs.entity.Beverage;
+
+public class StockControl{
 	
-	public void CalculateStock() {
-		
-		
+	private InMemoryDB b;
+	
+	public StockControl(InMemoryDB db) {
+		this.b = db;
 	}
+	public float CalculateStock() {
+		float sum = 0;
+		for (Beverage h : b.getDb()) {
+			sum += h.getPreco();
+			
+		}return sum;
+	}
+	public byte LevelStock() {
+		byte sum = 0;
+		for (Beverage h : b.getDb()) {
+			sum += h.getQtd();
+			
+		}
+		if (sum > 100) {
+			System.out.println("cheio");
+			return 100;
+		}
+		if (sum > 50) {
+			System.out.println("metade");
+			return sum;
+		}else {
+			System.out.println("vazio");
+			return sum;
+		}
+			
+	}
+	
 }
--- src/test/java/bvs/test/Stock/StockTest.java
+++ src/test/java/bvs/test/Stock/StockTest.java
@@ -2,15 +2,42 @@ package bvs.test.Stock;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import bvs.controle.InMemoryDB;
+import bvs.controle.StockControl;
+import bvs.entity.Beverage;
+import junit.framework.Assert;
 
 public class StockTest {
 	private bvs.controle.StockControl stock;
+	private ArrayList<Beverage> bebidas;
+	private InMemoryDB db;
+	
+	@Before
+	public void setUp() {
+		bebidas  = new ArrayList<Beverage>();
+		db = new InMemoryDB(bebidas);
+		stock = new StockControl(db);
+		
+		Beverage b1 = new Beverage(1, "Ninnoff", "Vodka", 2.50f, (short)20, 1, false);
+		Beverage b2 = new Beverage(2, "Fogo Verde", "Absinto", 3.40f,(short)12, 1, true);
+		
+		bebidas.add(b1);
+		bebidas.add(b2);
+	}
 	@Test
 	public void CalculateTheValueInRsTest() {
-		float sumexpected = 0;
 		float results = 2.50f + 3.40f;
-		assertEquals(results, sumexpected, 0.001);
+		assertEquals(results, stock.CalculateStock(), 0.001f);
 	}
-
+	@Test
+	public void LevelStockTest() {
+		
+		byte expecteds = 2;
+		assertEquals(expecteds, stock.LevelStock());
+	}
 }
