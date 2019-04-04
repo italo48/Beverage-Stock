d37e351e94b24ad2dc5d6af171f70379d168d22a
Ítalo O Costa <italo.de.oliveira01@gmail.com>
2019-04-03 23:17:40 -0300

    ignore no ghost test

--- src/test/java/bvs/test/AppTest.java
+++ src/test/java/bvs/test/AppTest.java
@@ -3,12 +3,13 @@ package bvs.test;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.Ignore;
 
+@Ignore("Hotfix para o ArchCI")
 public class AppTest {
 
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue( true );
-    }
-    
+    }   
 }
